367471e44dd7dd0a9e0c8073bf59256dabc61b79
Seo Hyunha <hyunhaseo@gmail.com>
2019-12-12 22:49:14 +0900

    Complete version 1

--- client/src/Complete.js
+++ client/src/Complete.js
@@ -1,34 +1,121 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Complete.css";
 import Title from "./components/Title";
 import { Link } from "react-router-dom";
 import "./components/Sticky.css";
+import axios from "axios";
+
+const API_URL = "http://192.168.43.238:8000";
+
+function formatter(time) {
+  if (time < 10) {
+    return "0" + time.toString();
+  }
+  return time;
+}
 
 function Complete() {
+  const [data, setData] = useState({});
+  const [titleText, setTitleText] = useState("");
+
+  useEffect(() => {
+    window.minute = window.minute ? window.minute : 0;
+
+    if (window.minute < window.timer) {
+      setTitleText("목표한 시간보다 빨리 먹었어요.");
+    } else {
+      setTitleText("목표한 시간에 맞춰 잘 먹었어요!");
+    }
+
+    setData({
+      ...data,
+      user: "dano",
+      real_meal_time: `00:${formatter(window.minute)}`
+    });
+  }, []);
+
+  async function handleSubmit() {
+    console.log(data);
+    await axios.post(`${API_URL}/barundano/mealregist/`, data).then(res => {
+      console.log(res.data);
+    });
+  }
+
   return (
     <div className="container-report">
-      <Title text="목표한 시간에 맞춰 천천히 먹기 성공!" />
+      <Title text={titleText} />
       <div className="food">
         <div className="box">
-          <strong>총 식사시간</strong> {window.minute}분 {window.second}초<br />
-          <strong>아침</strong> {window.foodName}
+          <strong>아침식사</strong> {window.foodName}
+          <br />
+          <strong>목표 식사시간</strong> {formatter(window.timer)}분
+          <br />
+          <strong>총 식사시간</strong> {formatter(window.minute)}분{" "}
         </div>
       </div>
       <div className="box">
         <strong>누구와 식사 하셨나요?</strong>
         <br />
-        <span>혼자</span> <span>함께</span>
+        <div
+          onClick={() => {
+            setData({ ...data, eat_together: 1 });
+          }}
+          className={data.eat_together === 1 ? "selected" : ""}
+        >
+          <div className="icon-wrap">
+            <img src="/images/emoji_2.png" alt="emoji" />
+          </div>
+          <p>혼자</p>
+        </div>
+        <div
+          onClick={() => {
+            setData({ ...data, eat_together: 2 });
+          }}
+          className={data.eat_together === 2 ? "selected" : ""}
+        >
+          <div className="icon-wrap">
+            <img src="/images/emoji_2.png" alt="emoji" />
+            <img src="/images/emoji_2.png" alt="emoji" />
+          </div>
+          <p>함께</p>
+        </div>
       </div>
       <div className="box">
         <strong>식사하는 동안 기분은 어떠셨나요?</strong>
         <br />
-        <div>별로에요</div>
-        <div>괜찮아요</div>
-        <div>좋아요</div>
+        <div className="emoji-wrap">
+          <div
+            onClick={() => {
+              setData({ ...data, feeling: 1 });
+            }}
+            className={data.feeling === 1 ? "selected" : ""}
+          >
+            <img src="/images/emoji_1.png" alt="emoji" />
+            <p>좋아요</p>
+          </div>
+          <div
+            onClick={() => {
+              setData({ ...data, feeling: 2 });
+            }}
+            className={data.feeling === 2 ? "selected" : ""}
+          >
+            <img src="/images/emoji_2.png" alt="emoji" />
+            <p>괜찮아요</p>
+          </div>
+          <div
+            onClick={() => {
+              setData({ ...data, feeling: 3 });
+            }}
+            className={data.feeling === 3 ? "selected" : ""}
+          >
+            <img src="/images/emoji_3.png" alt="emoji" />
+            <p>별로에요</p>
+          </div>
+        </div>
       </div>
-      <Link to="/report">
-        <div className="sticky">식사종료</div>
-      </Link>
+      <div className="sticky" onClick={handleSubmit}>
+        식사종료
+      </div>
     </div>
   );
 }
--- client/src/Timer.js
+++ client/src/Timer.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect, useInterval } from "react";
 import { Link } from "react-router-dom";
+import Title from './components/Title';
 import "./Timer.css";
 import "./components/Sticky.css";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 function formatter(time) {
   if (time < 10) {
@@ -35,11 +38,26 @@ function Timer() {
   return (
     <div className="container-timer">
       <div className={`blink ${flash ? "" : "hide"}`}></div>
+      <Title text={`목표시간: ${window.timer}분`} />
       <div className="timer-graphic">
-        <p>
-          {formatter(minute)}:{formatter(second)}
-        </p>
+        <CircularProgressbar
+          maxValue={window.timer * 60}
+          value={minute * 60 + second}
+          text={`
+          ${formatter(minute)}:${formatter(second)}`}
+          styles={buildStyles({
+            pathColor: `#f86767`,
+            textColor: "#f86767",
+            trailColor: "#eaeaea"
+          })}
+        />{" "}
       </div>
+      <p>
+        음식을 천천히 씹으며 타이머가 울릴 때까지
+        <br />
+        먹을 수 있도록 노력해보세요!
+      </p>
+
       <Link to="/complete">
         <div className="sticky">식사종료</div>
       </Link>
