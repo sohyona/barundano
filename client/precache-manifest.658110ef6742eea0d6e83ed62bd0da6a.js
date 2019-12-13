cf7bccebb544a00b735f55f9993221a3313841ce
Seo Hyunha <hyunhaseo@gmail.com>
2019-12-13 12:39:06 +0900

    Deploy

---
+++ client/precache-manifest.658110ef6742eea0d6e83ed62bd0da6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da08850962f336add3a9b1cd5ede0359",
+    "url": "/barundano-client/index.html"
+  },
+  {
+    "revision": "5445f0a9449b8dd51ab9",
+    "url": "/barundano-client/static/css/2.94bd41ed.chunk.css"
+  },
+  {
+    "revision": "5880dc52ec5e453206c5",
+    "url": "/barundano-client/static/css/main.9328e38b.chunk.css"
+  },
+  {
+    "revision": "5445f0a9449b8dd51ab9",
+    "url": "/barundano-client/static/js/2.817d728b.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/barundano-client/static/js/2.817d728b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5880dc52ec5e453206c5",
+    "url": "/barundano-client/static/js/main.0f4098b9.chunk.js"
+  },
+  {
+    "revision": "2ab0045069a2b186b39a",
+    "url": "/barundano-client/static/js/runtime-main.4dc0284e.js"
+  }
+]);
